71f691d1a381646265fc6009529e9f567e488c19
sumitbatra-nxp <107573027+sumitbatra-nxp@users.noreply.github.com>
2022-09-22 20:06:03 +0530

    Rddrone fmuk66 imu (#14)
    * Configure sensors in dts, add drive-open-drain to i2c pin settings
    
    * Update external i2c pin settings
    
    * Remove xml file
    
    * Remove vl53l0x as this is not an on-board device
    
    * Add patch from Sumit for gpio kernel priority
    
    * Ensuring VCC to 3.3V for sensors earlier than I2C and Sensor Init
    
    * Enable regulator drivers for board, remove code to pull gpio high
    
    * 1. Added support for fxos8700 sensor over SPI bus 2. Made the fxos driver APIs generic for I2C and SPI
    
    * 1. Added support for fxas21002 sensor over SPI bus 2. Made the fxas driver APIs generic for I2C and SPI
    
    * Fix for managing Chip select of SPI1 controller
    
    * 1. Resetting SPI slaves with RST pin and I2C with Control Register 2. Code Cleanup
    
    * Code Cleanup
    
    * 1. Added 2nd socketcan device to make 2 devices work in parallel 2. Added its Test code as well
    
    Co-authored-by: Landon Haugh <landon.haugh@nxp.com>
    Co-authored-by: Sumit Batra <sumit.batra@nxp.com>

--- drivers/sensor/fxas21002/fxas21002_trigger.c
+++ drivers/sensor/fxas21002/fxas21002_trigger.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define DT_DRV_COMPAT nxp_fxas21002
+
 #include <zephyr/logging/log.h>
 
 #include "fxas21002.h"
@@ -55,7 +57,7 @@ static void fxas21002_handle_int(const struct device *dev)
 
 	k_sem_take(&data->sem, K_FOREVER);
 
-	if (i2c_reg_read_byte_dt(&config->i2c, FXAS21002_REG_INT_SOURCE,
+	if (config->ops->byte_read(dev, FXAS21002_REG_INT_SOURCE,
 				 &int_source)) {
 		LOG_ERR("Could not read interrupt source");
 		int_source = 0U;
@@ -132,7 +134,7 @@ int fxas21002_trigger_set(const struct device *dev,
 	}
 
 	/* Configure the sensor interrupt */
-	if (i2c_reg_update_byte_dt(&config->i2c, FXAS21002_REG_CTRLREG2, mask,
+	if (config->ops->reg_field_update(dev, FXAS21002_REG_CTRLREG2, mask,
 				   handler ? mask : 0)) {
 		LOG_ERR("Could not configure interrupt");
 		ret = -EIO;
@@ -177,14 +179,13 @@ int fxas21002_trigger_init(const struct device *dev)
 #elif defined(CONFIG_FXAS21002_TRIGGER_GLOBAL_THREAD)
 	data->work.handler = fxas21002_work_handler;
 #endif
-
 	/* Route the interrupts to INT1/INT2 pins */
 	ctrl_reg2 = 0U;
 #if CONFIG_FXAS21002_DRDY_INT1
 	ctrl_reg2 |= FXAS21002_CTRLREG2_CFG_DRDY_MASK;
 #endif
 
-	if (i2c_reg_write_byte_dt(&config->i2c, FXAS21002_REG_CTRLREG2,
+	if (config->ops->byte_write(dev, FXAS21002_REG_CTRLREG2,
 				  ctrl_reg2)) {
 		LOG_ERR("Could not configure interrupt pin routing");
 		return -EIO;
